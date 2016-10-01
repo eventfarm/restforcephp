482647b0b5e36d5a06f2e2f84777f723f17ffb33
Jason Raimondi <jason@raimondi.us>
2016-10-01 13:11:48 -0700

    Add git hooks.

---
+++ bin/copy-hooks.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cp bin/post-update .git/hooks/
+cp bin/post-merge .git/hooks/
+cp bin/update-composer.sh .git/hooks/
---
+++ bin/update-composer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Run composer install when composer.lock is changed
+
+changed_files="$(git diff-tree -r --name-only --no-commit-id ORIG_HEAD HEAD)"
+
+check_run() {
+	echo "$changed_files" | grep --quiet "$1" && eval "$2"
+}
+
+check_run composer.lock "composer install"
