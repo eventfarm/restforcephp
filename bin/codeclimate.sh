57021d23c26178da383918b591e0acadd5bfa1ad
Jason Raimondi <jason@raimondi.us>
2016-10-03 15:04:13 -0700

    Auto deploy to code climate for code coverage.

---
+++ bin/codeclimate.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+
+rm -f codeclimate.json
+xphp70 vendor/bin/test-reporter --stdout > codeclimate.json
+curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
