09fc39f33750e51ae7d2d45419131381b7614e96
Jason Raimondi <jason@raimondi.us>
2016-10-02 18:36:02 -0700

    Add test for Guzzle Rest Client. 100% Covered.

---
+++ tests/GuzzleRestClientTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Jmondi\Restforce;
+
+use Jmondi\Restforce\RestClient\GuzzleRestClient;
+use Mockery;
+use Psr\Http\Message\ResponseInterface;
+
+class GuzzleRestClientTest extends \PHPUnit_Framework_TestCase
+{
+    public function testGuzzleRestClientFacade()
+    {
+        $client = Mockery::mock(\GuzzleHttp\Client::class);
+        $response = Mockery::mock(ResponseInterface::class);
+
+        $guzzleRestClient = new GuzzleRestClient(
+            $client
+        );
+
+        $client->shouldReceive('request')
+        ->andReturn($response);
+
+        $guzzleRestClient->request('GET', 'limits');
+    }
+}
