95133faf92b7c44e37f16f02ef597156720c35e2
Mark Faga <mark@attend.com>
2016-10-06 09:47:20 -0400

    Additional validation on test asserts

--- tests/RestforceClientTest.php
+++ tests/RestforceClientTest.php
@@ -1,11 +1,8 @@
 <?php
 namespace Jmondi\Restforce;
 
-use GuzzleHttp\Psr7\Response;
-use Jmondi\Restforce\Oauth\AccessTokenInterface;
 use Jmondi\Restforce\Oauth\SalesforceProviderInterface;
 use Jmondi\Restforce\RestClient\RestClientInterface;
-use Jmondi\Restforce\TokenRefreshInterface;
 use Mockery;
 use Psr\Http\Message\ResponseInterface;
 
@@ -212,7 +209,8 @@ class RestforceClientTest extends \PHPUnit_Framework_TestCase
                 $this->assertEquals($endpoint, $e);
                 $this->assertEquals($options, $o);
                 return $response;
-            });
+            })
+            ->once();
 
         return RestforceClient::with(
             $restClient,
