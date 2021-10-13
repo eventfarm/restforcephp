a677e3e98734a1b8d92acb0f63292469721afd3b
Florian Duc <fduc@template-stretch64.dc.vpg.lan>
2021-10-13 18:26:19 +0200

    add bong city model

---
+++ src/Types/BongCity.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace EventFarm\Restforce\Types;
+
+/**
+ * Class City Type Salesforce
+ *
+ * @package EventFarm\Restforce\Types
+ * @author  Florian Duc <fduc@voyageprive.com>
+ */
+class BongCity
+{
+    const SF_OBJECT = 'BongCity__c';
+
+    const SF_ID = 'Id';
+    const SF_NAME = 'Name';
+    const SF_ID_BONG = 'ID_BONG__c';
+    const SF_COUNTRY_NAME = 'CountryName__c';
+
+    /**
+     * Query fields available in SF for a BongCity
+     */
+    const SF_FIELD_LIST = [
+        self::SF_ID,
+        self::SF_NAME,
+        self::SF_ID_BONG
+    ];
+}
