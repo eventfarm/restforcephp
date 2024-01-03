abf53e46a0526dbe40b05a9a979cf8b52e2fba2c
Todd Cornett <tcornett@gmail.com>
2024-01-02 18:17:49 -0800

    Removing out of date CodeClimate plugin

--- bin/codeclimate.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-rm -f codeclimate.json
-php vendor/bin/test-reporter --stdout > codeclimate.json
-curl -X POST -d @codeclimate.json -H 'Content-Type: application/json' -H 'User-Agent: Code Climate (PHP Test Reporter v0.1.1)' https://codeclimate.com/test_reports
